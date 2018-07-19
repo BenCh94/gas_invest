5f338b951d54615fe2408a409476d4988715fc2a
Ben <bchadwick94@gmail.com>
2018-07-19 22:43:23 +0100

    adding intial models and admin configs

--- gas_dash/models.py
+++ gas_dash/models.py
@@ -1,21 +1,32 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Stock(models.Model):
-	name = models.CharField()
+	StockStatuses = [('a', 'Active'), ('i', 'Inactive')]
+	name = models.CharField(max_length=200)
+	ticker = models.CharField(max_length=10)
 	quantity = models.IntegerField()
+	invested = models.FloatField()
+	fees_usd = models.FloatField()
+	start_date = models.DateTimeField()
+	status = models.CharField(max_length=2, choices=StockStatuses)
+	def __str__(self):
+		return self.name
+
 
 
 class Trade(models.Model):
-	stock = models.ForeignKey(Stock)
+	TradeTypes = [('b', 'Buy'), ('s','Sell')]
+	stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+	date = models.DateTimeField()
+	trade_type = models.CharField(max_length=2, choices=TradeTypes)
+	amount = models.FloatField()
+	fees_usd = models.FloatField()
+	avg_price = models.FloatField()
+	def __str__(self):
+		return self.avg_price
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
