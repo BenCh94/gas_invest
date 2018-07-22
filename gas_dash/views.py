9acf8de32211cea481b617f52a9d88dd08c532d0
Ben <bchadwick94@gmail.com>
2018-07-22 19:45:41 +0100

    views updated

--- gas_dash/views.py
+++ gas_dash/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Stock
 
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello World!')
+	stocks = Stock.objects.all()
+	context = {'stocks': stocks}
+	return render(request, 'gas_dash/dashboard.html', context)
+
+def stock(request, stock_id):
+	stock = get_object_or_404(Stock, pk=stock_id)
+	return render(request, 'gas_dash/stock_detail.html', {'stock': stock})
+
+def trades(request, stock_id):
+	return HttpResponse("You're looking at trades for %s." % stock_id)
+
+def trade(request, trade_id):
+	return HttpResponse("You're looking at trade %s." % trade_id)
